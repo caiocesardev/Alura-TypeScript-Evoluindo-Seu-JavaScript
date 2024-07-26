98b9b5d209a9ffb0e51da1f52af9a194b08bae1f
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-07-26 11:36:53 -0300

    readonly

--- dist/js/models/Negociacao.js
+++ dist/js/models/Negociacao.js
@@ -1,17 +1,8 @@
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-    get data() {
-        return this.data;
-    }
-    get quantidade() {
-        return this.quantidade;
-    }
-    get valor() {
-        return this.valor;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
         return this.quantidade * this.valor;
