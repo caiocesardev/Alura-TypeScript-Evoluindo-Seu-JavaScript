f30002125b60619678db00f5bb7b94310f4717e4
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-08-09 22:58:52 -0300

    removendo comentarios do codigo

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -4,6 +4,8 @@ import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
+/* Nelson passou por aqui */
+
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
