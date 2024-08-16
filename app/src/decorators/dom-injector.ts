ae9191f573d46ba32257738b9a18fda4590e9333
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-08-15 23:18:12 -0300

    03.03.05 Utilizando Object.defineProperty

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,9 +1,19 @@
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
+        console.log(`Modificando prototype ${target.constructor.name}
+             e adicionando getter para a propriedade ${propertyKey}`);
         const getter = function() {
             const elemento = document.querySelector(seletor);
+            console.log(`buscando elemento do DOm com o seletor 
+                ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         }
+
+        Object.defineProperty(
+            target, 
+            propertyKey,
+            { get: getter}
+        );
     }
 }
 
